314d2de89ca684c7bb8d9fc7ee42110e1d242e61
zhendrix <zhendrix@my.wctc.edu>
2023-10-03 14:22:44 -0500

    [1.10] Input user choice - Display choices to user - Input user choice

--- Program.cs
+++ Program.cs
@@ -6,9 +6,19 @@ string path = Directory.GetCurrentDirectory() + "\\nlog.config";
 // create instance of Logger
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 logger.Info("Program started");
+
 string movieFilePath = Directory.GetCurrentDirectory() + "\\movies.csv";
-
 MovieFile movieFile = new MovieFile(movieFilePath);
-
+string choice = "";
+do
+{
+  // display choices to user
+  Console.WriteLine("1) Add Movie");
+  Console.WriteLine("2) Display All Movies");
+  Console.WriteLine("Enter to quit");
+  // input selection
+  choice = Console.ReadLine();
+  logger.Info("User choice: {Choice}", choice);
+} while (choice == "1" || choice == "2");
 
 logger.Info("Program ended");
